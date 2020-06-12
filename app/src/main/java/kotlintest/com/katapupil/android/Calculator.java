63a2ef22ccfa818ce833f73137a7d4d31c2df9b4
Janos Szidor <janos.szidor@moon42.com>
2020-06-12 22:57:11 +0200

    Added calculator related activity and layout shells.

---
+++ app/src/main/java/kotlintest/com/katapupil/android/Calculator.java
@@ -0,0 +1,13 @@
+package kotlintest.com.katapupil.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class Calculator extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
